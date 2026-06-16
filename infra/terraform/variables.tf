43fb79b005293462f02370cd305a35be9b028796
wincvh <vincent.huang@weris-inc.com>
2026-06-16 16:04:04 -0400

    Configure Terraform for us-east-2 deployment

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,4 +1,23 @@
-variable "aws_region" { type = string default = "us-east-1" }
-variable "github_owner" { type = string }
-variable "github_repo" { type = string }
-variable "bucket_name" { type = string }
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "github_owner" {
+  type    = string
+  default = "wincvh"
+}
+
+variable "github_repo" {
+  type    = string
+  default = "agent"
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "app_name" {
+  type    = string
+  default = "agent"
+}

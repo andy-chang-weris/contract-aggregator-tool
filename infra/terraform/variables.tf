b057525ee4da4c80eea6d21919b7b6b6daca511b
wincvh <vincent.huang@weris-inc.com>
2026-06-16 15:58:00 -0400

    Add Terraform variables

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region" { type = string default = "us-east-1" }
+variable "github_owner" { type = string }
+variable "github_repo" { type = string }
+variable "bucket_name" { type = string }

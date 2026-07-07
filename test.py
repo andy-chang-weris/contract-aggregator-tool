2e1f5e600ea82761f01918bd8c6785e97d1b7f96
andy-chang-weris <andy.chang@weris-inc.com>
2026-07-07 11:23:41 -0400

    removed test files

--- test.py
+++
@@ -1,7 +0,0 @@
-# test_acq.py
-from sources.acq_gateway import fetch_and_parse
-
-postings = fetch_and_parse()
-print(f"\nTotal returned: {len(postings)}")
-if postings:
-    print("Sample:", postings[0])
